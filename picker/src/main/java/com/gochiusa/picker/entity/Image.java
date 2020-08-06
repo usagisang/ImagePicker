07ea9c3414aca61d43d80686230b6c379849bbb0
Liu Zhengjie <1023192250@qq.com>
2020-08-06 22:28:28 +0800

    feat(com.gochiusa.picker.entity):添加了需要使用的实体类

---
+++ picker/src/main/java/com/gochiusa/picker/entity/Image.java
@@ -0,0 +1,62 @@
+package com.gochiusa.picker.entity;
+
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+public class Image {
+    private long id;
+    private String mimeType;
+    private Uri uri;
+    private long size;
+
+    public Image() {}
+
+    public Image(long id, String mimeType, long size) {
+        this.id = id;
+        this.mimeType = mimeType;
+        this.size = size;
+        createUri();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public Uri getUri() {
+        return uri;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+        createUri();
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    private void createUri() {
+        // 根据id，初始化能打开这个Image的Uri
+        uri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+    }
+
+    public static Image valueOf(Cursor cursor) {
+        return new Image(cursor.getLong(cursor.getColumnIndex(MediaStore.Files.FileColumns._ID)),
+                cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.MIME_TYPE)),
+                cursor.getLong(cursor.getColumnIndex(MediaStore.MediaColumns.SIZE)));
+    }
+}
--- picker/src/main/java/com/gochiusa/picker/entity/ImageRequest.java
+++ picker/src/main/java/com/gochiusa/picker/entity/ImageRequest.java
@@ -2,12 +2,32 @@ package com.gochiusa.picker.entity;
 
 
 import com.gochiusa.picker.R;
+import com.gochiusa.picker.engine.ImageEngine;
+import com.gochiusa.picker.engine.impl.DefaultEngine;
 
 public final class ImageRequest {
 
+    /**
+     *  是否为多选模式
+     */
     public boolean countable;
+    /**
+     *  可以选择的最大数量
+     */
     public int maxSelectable;
+    /**
+     *  框架整体使用的主题的资源id
+     */
     public int themeId;
+    /**
+     *  标记是否为后进先加载，默认为先进先加载
+     */
+    private boolean LIFO;
+
+    /**
+     *  图片加载引擎
+     */
+    private ImageEngine imageEngine;
 
     private ImageRequest() {}
 
@@ -24,6 +44,26 @@ public final class ImageRequest {
         countable = false;
         maxSelectable = 1;
         themeId = R.style.PickerTheme;
+        LIFO = false;
+    }
+
+    public ImageEngine getImageEngine() {
+        if (imageEngine == null) {
+            imageEngine = new DefaultEngine(LIFO);
+        }
+        return imageEngine;
+    }
+
+    public void setImageEngine(ImageEngine imageEngine) {
+        this.imageEngine = imageEngine;
+    }
+
+    public void setLIFO(boolean LIFO) {
+        this.LIFO = LIFO;
+    }
+
+    public boolean isLIFO() {
+        return LIFO;
     }
 
     private static final class InstanceHolder {
