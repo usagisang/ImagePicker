41309f980c092f713b07ffd0e255a11f632c78d2
Liu Zhengjie <1023192250@qq.com>
2020-08-07 21:56:41 +0800

    feat:添加自定义的正方形布局

---
+++ picker/src/main/java/com/gochiusa/picker/ui/widget/SquareFrameLayout.java
@@ -0,0 +1,24 @@
+package com.gochiusa.picker.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class SquareFrameLayout extends FrameLayout {
+    public SquareFrameLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public SquareFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // 全部使用宽度模式交由父类测量
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
