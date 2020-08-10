617076bd651f30e5e5740006de54962bd1b6b6c7
Liu Zhengjie <1023192250@qq.com>
2020-08-10 14:15:33 +0800

    fix(com.gochiusa.picker.ui.widget):修复了可缩放的ImageView滑动事件与ViewPager父布局的冲突，并添加了滑动惯性

--- picker/src/main/java/com/gochiusa/picker/ui/widget/ScalableImageView.java
+++ picker/src/main/java/com/gochiusa/picker/ui/widget/ScalableImageView.java
@@ -1,10 +1,12 @@
 package com.gochiusa.picker.ui.widget;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 
 import androidx.appcompat.widget.AppCompatImageView;
@@ -20,6 +22,8 @@ public class ScalableImageView extends AppCompatImageView {
      *  可拖拽的标志变量
      */
     private boolean mCanDrag = false;
+
+    private static final float FLING_DAMPING_FACTOR = 0.9f;
 
     /**
      *  允许的最大缩放倍数
@@ -60,23 +64,47 @@ public class ScalableImageView extends AppCompatImageView {
      */
     private float mNowScale = 1f;
 
+    /**
+     *  手势检测器
+     */
+    private GestureDetector mGestureDetector;
+
+    private FlingAnimator mFlingAnimator;
+
     public ScalableImageView(Context context) {
         super(context);
         setScaleType(ScaleType.MATRIX);
+        initGestureDetector();
     }
     public ScalableImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setScaleType(ScaleType.MATRIX);
+        initGestureDetector();
     }
     public ScalableImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setScaleType(ScaleType.MATRIX);
+        initGestureDetector();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         initImagePositionAndSize();
+    }
+
+
+    /**
+     *  重写该方法，以解决滑动冲突，当图片尚未到达横向边界，说明仍可拖拽
+     */
+    @Override
+    public boolean canScrollHorizontally(int direction) {
+        refreshImageRect();
+        if (direction > 0) {
+            return mImageRect.right > getWidth();
+        } else {
+            return mImageRect.left < 0;
+        }
     }
 
     /**
@@ -87,7 +115,7 @@ public class ScalableImageView extends AppCompatImageView {
         // 初始化ImageRect
         refreshImageRect();
         // 判断能否进行计算，如果满足下列条件，是无法计算缩放的结果的
-        boolean notCalculate = (getMeasuredWidth() == 0) || (getMeasuredHeight() == 0) ||
+        boolean notCalculate = (getWidth() == 0) || (getHeight() == 0) ||
                 (mImageRect.height() == 0) || (mImageRect.width() == 0);
         if (notCalculate) {
             return;
@@ -95,8 +123,8 @@ public class ScalableImageView extends AppCompatImageView {
         // 设置几何中心
         mImageMidden.set(mImageRect.centerX(), mImageRect.centerY());
         // 计算一个比较合适的缩放比例，对图片进行初始化缩放
-        mMinScale = Math.min(getMeasuredWidth() / mImageRect.width(),
-                getMeasuredHeight() / mImageRect.height());
+        mMinScale = Math.min(getWidth() / mImageRect.width(),
+                getHeight() / mImageRect.height());
         mNowScale = mMinScale;
         // 缩放
         mMatrix.postScale(mMinScale, mMinScale, mImageRect.centerX(), mImageRect.centerY());
@@ -108,10 +136,38 @@ public class ScalableImageView extends AppCompatImageView {
         commitMatrix();
     }
 
+    /**
+     *  初始化手势检测器和滑动屏幕的辅助类
+     */
+    private void initGestureDetector() {
+        GestureDetector.SimpleOnGestureListener listener =
+                new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2,
+                                   float velocityX, float velocityY) {
+                // 只允许单手指拖动
+                if (mCanDrag) {
+                    // 取消正在进行的所有动画
+                    cancelAllAnimator();
+                    // 创建新的滑动动画
+                    mFlingAnimator = new FlingAnimator(velocityX / 60,
+                            velocityY / 60);
+                    mFlingAnimator.start();
+                }
+                // 刷新界面
+                invalidate();
+                return true;
+            }
+        };
+        mGestureDetector = new GestureDetector(getContext(), listener);
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN :
+                // 单指按下，则取消所有动画
+                cancelAllAnimator();
             case MotionEvent.ACTION_POINTER_DOWN : {
                 // 计算本次触控事件的中点
                 calculateMidPointOfFinger(event);
@@ -150,6 +206,8 @@ public class ScalableImageView extends AppCompatImageView {
                 break;
             }
         }
+        // 交给手势控制器处理事件
+        mGestureDetector.onTouchEvent(event);
         return true;
     }
 
@@ -183,6 +241,7 @@ public class ScalableImageView extends AppCompatImageView {
         return (float) Math.sqrt(dx * dx + dy * dy);
     }
 
+
     /**
      *  平移操作
      */
@@ -193,6 +252,53 @@ public class ScalableImageView extends AppCompatImageView {
         // 重置中点信息
         mLastMidPoint.set(event.getX(), event.getY());
         mMatrix.postTranslate(dx, dy);
+    }
+
+    /**
+     *  将图片在X轴与Y轴下滑动若干距离，使用Matrix变换实现
+     * @param dx 需要在X轴下滑行的距离
+     * @param dy 需要在Y轴下滑行的距离
+     * @return 是否在X轴或者Y轴方向成功滑动图片
+     */
+    private boolean scrollViewByDistance(float dx, float dy) {
+        refreshImageRect();
+        int viewWidth = getWidth();
+        int viewHeight = getHeight();
+        // 接下来开始检查一些边界条件，以确定是否干涉滑动距离dx与dy
+
+        // 如果当前图片宽度小于控件宽度，或超过了左、右边界，X轴上不允许滑动，重置X轴滑动距离
+        if (mImageRect.width() < viewWidth || mImageRect.left >= 0 ||
+                mImageRect.right < viewWidth) {
+            dx = 0;
+        // 如果图片左边在滑动后超出左边界
+        } else if (mImageRect.left + dx > 0) {
+            // X轴滑动距离重置为恰好到达左边界的距离
+            dx = - mImageRect.left;
+
+        // 如果图片右边在滑动后超出右边界
+        } else if (mImageRect.right + dx < viewWidth) {
+            // X轴滑动距离重置为恰好到达右边界的距离
+            dx = viewWidth - mImageRect.right;
+
+        }
+        // 如果当前图片高度小于控件高度，或超过了上、下边界，Y轴上不允许滑动，重置X轴滑动距离
+        if (mImageRect.height() < viewHeight || mImageRect.top > 0 ||
+                mImageRect.bottom < viewHeight) {
+            dy = 0;
+        // 如果图片上边缘在滑动后超出上边界
+        } else if (mImageRect.top + dy > 0) {
+            // Y轴滑动距离重置为恰好到达上边界的距离
+            dy = - mImageRect.top;
+        // 如果图片下边缘在滑动后超出下边界
+        } else if (mImageRect.bottom + dy < viewHeight) {
+            // Y轴滑动距离重置为恰好到达下边界的距离
+            dy = viewHeight - mImageRect.bottom;
+        }
+        // 提交滑动
+        mMatrix.postTranslate(dx, dy);
+        commitMatrix();
+        // 检查是否发生滑动
+        return dx != 0 || dy != 0;
     }
 
     /**
@@ -286,6 +392,61 @@ public class ScalableImageView extends AppCompatImageView {
         if (getDrawable() != null) {
             mImageRect.set(getDrawable().getBounds());
             mMatrix.mapRect(mImageRect, mImageRect);
+        }
+    }
+
+    /**
+     *  尝试取消正在进行的所有动画
+     */
+    private void cancelAllAnimator() {
+        if (mFlingAnimator != null) {
+            mFlingAnimator.cancel();
+            mFlingAnimator = null;
+        }
+    }
+
+    /**
+     * 惯性动画
+     *
+     * 速度逐渐衰减,每帧速度衰减为原来的FLING_DAMPING_FACTOR,当速度衰减到小于1时停止.
+     * 当图片不能移动时,动画停止.
+     */
+    private class FlingAnimator extends ValueAnimator
+            implements ValueAnimator.AnimatorUpdateListener {
+
+        /**
+         * 缓存每一帧X轴与Y轴的速度
+         */
+        private float[] mVelocity;
+
+        /**
+         * 初始滑动速度参数单位必须为 像素/帧，否则会导致滑动过快的问题
+         * @param velocityX X轴的初始滑动速度
+         * @param velocityY Y轴的初始滑动速度
+         */
+        public FlingAnimator(float velocityX, float velocityY) {
+            super();
+            // 设置属性值从0到1变化，但其实没有再使用这个属性
+            setFloatValues(0, 1f);
+            // 持续时间设置得比较大，必须通过手动的方式停止动画
+            setDuration(1000000);
+            addUpdateListener(this);
+            mVelocity = new float[] {velocityX, velocityY};
+        }
+
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            float velocityX = mVelocity[0];
+            float velocityY = mVelocity[1];
+            // 移动图像、返回是否移动的布尔值
+            boolean result = scrollViewByDistance(velocityX, velocityY);
+            // 将速度按照比例衰减
+            mVelocity[0] *= FLING_DAMPING_FACTOR;
+            mVelocity[1] *= FLING_DAMPING_FACTOR;
+            // 速度过小，或者X轴与Y轴都不能移动，结束动画
+            if (! result || Math.sqrt(velocityX * velocityX + velocityY * velocityY) < 1f) {
+                animation.cancel();
+            }
         }
     }
 }
